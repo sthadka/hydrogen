1950e9539ff9001f3beaea711572eac086a9aaa1
Sukumar Yethadka <sukumar@thinkapi.com>
2014-04-28 17:56:03 +0200

    Add hydrogen_node
    Node related helper methods

---
+++ src/hydrogen_node.erl
@@ -0,0 +1,22 @@
+%%% -----------------------------------------------------------------------------
+%%% @doc hydrogen_node
+%%%
+%%% Node helper functions
+%%% @end
+
+-module(hydrogen_node).
+
+-include("hydrogen_convert.hrl").
+
+-export([hostname/0, hostname/1]).
+
+hostname() ->
+    hostname(node()).
+
+hostname(Node) ->
+    case string:tokens(?TO_L(Node), "@") of
+        [_NodeName, Hostname] ->
+            Hostname;
+        _ ->
+            []
+    end.
